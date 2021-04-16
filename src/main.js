b23f5dcddd13dcdfd0e717347fe0b3375ccdf258
ana-225 <ana-225@gmail.com>
2021-04-15 23:50:12 -0400

    se agregan filtro por tipos y se agregan metodo sort para ordenar de mayor a menor segun estadistica de base ataque

--- src/main.js
+++ src/main.js
@@ -10,13 +10,14 @@ function showMenu() {
 }
 // captura boton tipo
 const buttonType = document.querySelector('#type')
-// evento para mostrar vista de tipos
-buttonType.addEventListener('click', showType)
+
+
 // mostrar tipos
 function showType() {
   document.querySelector('#menuType').style.display = 'block'
   document.querySelector('#page2').style.display = 'none'
 }
+buttonType.addEventListener('click', showType)
 // mostrar cartilla de pokemones
 // document.querySelector('#search').addEventListener('click',letterPokemon)
 
@@ -24,59 +25,102 @@ function showType() {
 //   document.querySelector("#menu_name").style.display = "block"
 //   document.querySelector("#page2").style.display = "none"
 // }
-
+const typesPokemon = () => {
+  document.querySelector('#types').style.display = 'block'
+  document.querySelector('#menuType').style.display = 'none'
+}
 document.querySelector('#menuType').addEventListener('click', typesPokemon)
 
-function typesPokemon(){
-  document.querySelector('#types').style.display='block'
-  document.querySelector('#menuType').style.display='none'
-}
-// captura boton ataque y evento que muestra vista de ataque
-document.querySelector('#attack').addEventListener('click', menuAttack)
+
 
 // funcion mostar vista de ataque
-function menuAttack() {
-    document.querySelector('#menu_attack').style.display = 'block'
+const menuAttack = () => {
+  document.querySelector('#menu_attack').style.display = 'block'
   document.querySelector('#page2').style.display = 'none'
 }
+document.querySelector('#attack').addEventListener('click', menuAttack)
 
-document.getElementById("button_water").addEventListener("click",()=>{
-// //paso uno: filtrar informacion
- const buttonType= document.getElementById("menuType");
-  
-  
 const pokemonData = pokemon.pokemon;
 
- const type_pokemonWater = pokemonData.filter(pokemonData => pokemonData.type.includes("water"));
-//paso 2:pintar esa informacion filtrada 
+//traer para los types los datos que necesito son la imagen,nombre y numero. 
+//prueba para LOS TIPOS
+const buttonP = document.querySelectorAll(".button_p")
+
+buttonP.forEach(e => {
+  e.addEventListener("click", event => {
+    const buttonP = event.currentTarget.id;
+    //por que es diferente usar target y currentTarget...por q por ejemplo con boton water no funciona ...
+
+    document.querySelector("#typeTitle").innerHTML = "Tipo " + event.currentTarget.textContent
+
+    const types_pokemons = pokemonData.filter(pokemonData => pokemonData.type.includes(buttonP));
+    console.log(types_pokemons)
+
+    for (let i = 0; i < types_pokemons.length; i++) {
+
+      let card = document.createElement("div")
+      card.className = "styleCard"
+
+      let img_poke = document.createElement("img")
+      img_poke.src = types_pokemons[i].img
+      let infoCard = document.createElement("div")
+      let titlePokemon = document.createElement("h2")
+      titlePokemon.textContent = types_pokemons[i].name
+     
+      let num_poke = document.createElement("h3")
+      num_poke.textContent = types_pokemons[i].num
+
+      infoCard.appendChild(titlePokemon)
+      infoCard.appendChild(num_poke)
+      card.appendChild(img_poke)
+      card.appendChild(infoCard)
+
+      document.querySelector("#cardType").appendChild(card)
+
+      console.log(types_pokemons[i].name);
+    }
+  })
+
+});
+
+const attack = pokemonData.sort((a, b) => {
+  return b.stats['base-attack'] - a.stats['base-attack'];
+})
+console.log(attack)
+const viewAttack = document.querySelector("#list_attack");
+
+for (let i=0; i<attack.length;i++){
+  let card = document.createElement("div")
+      card.className = "attackCard"
+  let num_poke= document.createElement("h3")
+  num_poke.textContent= "N° "+attack[i].num
+  let img_attack= document.createElement("img")
+  img_attack.src= attack[i].img
  
- for(let i=0;i<type_pokemonWater.length;i++){
-   let titlePokemon=document.createElement("h1")
-   titlePokemon.textContent=type_pokemonWater[i].name
-   let list= document.getElementById("types")
-   list.appendChild(titlePokemon)
+  let litleAttack = document.createElement("h2")
+  litleAttack.textContent = attack[i].name
+   let num_attack= document.createElement("h3")
+  num_attack.textContent="Base Ataque : " + attack[i].stats['base-attack'] 
+  
+  card.appendChild(num_poke)
+  card.appendChild(img_attack)
+  card.appendChild(litleAttack)
+  card.appendChild(num_attack)
 
-   console.log(type_pokemonWater[i].name);
- }
- });
+  viewAttack.appendChild(card)
+  
+  console.log(litleAttack)
+  console.log(img_attack)
+}
 
-// let attack= pokemonData.map(x => x.stats['base-attack']);
-// console.log(attack);
-// 
-// attack.sort(orderAttack)
-// function orderAttack(a, b) {
-//   return b - a
-// }
-// console.log(attack);
-
-// //(captura el valor que se ingresa al input)
+//(captura el valor que se ingresa al input)
 document.querySelector("#searchName").addEventListener("keyup", (event) => {
 
   const listaFiltrada = pokemonData.filter(poke => poke.name.startsWith(event.target.value))
   let list2 = document.getElementById("list_pokemon")
   list2.innerHTML = ''
 
- // console.log(listaFiltrada)
+  console.log(listaFiltrada)
 
   for (let i = 0; i < listaFiltrada.length; i++) {
     let list_name = document.createElement("h2")
@@ -92,4 +136,4 @@ document.querySelector("#searchName").addEventListener("keyup", (event) => {
     //console.log(listaFiltrada[i].name);
   }
 }
-);
+)
