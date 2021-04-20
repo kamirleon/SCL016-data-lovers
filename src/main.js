ac428f8801259565f0b487042ae3efe4e5e34132
ana-225 <ana-225@gmail.com>
2021-04-19 22:23:43 -0400

    Se agrega imagen de flecha para volver a la pagina anterior

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ function showMenu() {
 // captura boton tipo
 const buttonType = document.querySelector('#type')
 
-
 // mostrar tipos
 function showType() {
   document.querySelector('#menuType').style.display = 'block'
@@ -25,6 +24,16 @@ buttonType.addEventListener('click', showType)
 //   document.querySelector("#menu_name").style.display = "block"
 //   document.querySelector("#page2").style.display = "none"
 // }
+
+// const buttonArrow= document.getElementById('arrow')
+
+// function arrowReturn() {
+//   document.getElementById('menuType').style.display= 'block'
+//   document.getElementById('types').style.display= 'none'
+
+// }
+// buttonArrow.addEventListener('click', arrowReturn)
+
 const typesPokemon = () => {
   document.querySelector('#types').style.display = 'block'
   document.querySelector('#menuType').style.display = 'none'
@@ -40,45 +49,40 @@ const menuAttack = () => {
 }
 document.querySelector('#attack').addEventListener('click', menuAttack)
 
-
 const pokemonData = pokemon.pokemon;
 
-//traer para los types los datos que necesito son la imagen,nombre y numero. 
-//prueba para LOS TIPOS
-const buttonP = document.querySelectorAll(".button_p")
+const buttonP = document.querySelectorAll('.button_p')
 
 buttonP.forEach(e => {
-  e.addEventListener("click", event => {
-    const buttonP = event.currentTarget.id;
-    //por que es diferente usar target y currentTarget...por q por ejemplo con boton water no funciona ...
+  e.addEventListener('click', event => {
 
-    document.querySelector("#typeTitle").innerHTML = "Tipo  " + event.currentTarget.textContent
+    const buttonP = event.currentTarget.id;
+    document.querySelector('#typeTitle').innerHTML = 'Tipo  ' + event.currentTarget.textContent
 
     const types_pokemons = pokemonData.filter(pokemonData => pokemonData.type.includes(buttonP));
     console.log(types_pokemons)
 
+
     for (let i = 0; i < types_pokemons.length; i++) {
 
-      let card = document.createElement("div")
-      card.className = "styleCard"
+      const card = document.createElement('div')
+      card.className = 'styleCard'
 
-      let img_poke = document.createElement("img")
+      let img_poke = document.createElement('img')
       img_poke.src = types_pokemons[i].img
-      let infoCard = document.createElement("div")
-      let titlePokemon = document.createElement("h2")
+      let infoCard = document.createElement('div')
+      let titlePokemon = document.createElement('h2')
       titlePokemon.textContent = types_pokemons[i].name
-     
-      let num_poke = document.createElement("h3")
-      num_poke.textContent = types_pokemons[i].num
+
+      let num_poke = document.createElement('h3')
+      num_poke.textContent = 'N° ' + types_pokemons[i].num
 
       infoCard.appendChild(titlePokemon)
       infoCard.appendChild(num_poke)
       card.appendChild(img_poke)
       card.appendChild(infoCard)
 
-      document.querySelector("#cardType").appendChild(card)
-
-      console.log(types_pokemons[i].name);
+      document.querySelector('#cardType').appendChild(card)
     }
   })
 
@@ -87,59 +91,54 @@ buttonP.forEach(e => {
 const attack = pokemonData.sort((a, b) => {
   return b.stats['base-attack'] - a.stats['base-attack'];
 })
-console.log(attack)
-const viewAttack = document.querySelector("#list_attack");
 
-for (let i=0; i<attack.length;i++){
-  let card = document.createElement("div")
-      card.className = "attackCard"
-  let num_poke= document.createElement("h4")
-  num_poke.textContent= "N° "+attack[i].num
-  let img_attack= document.createElement("img")
-  img_attack.src= attack[i].img
- 
-  let litleAttack = document.createElement("h2")
+const viewAttack = document.querySelector('#list_attack');
+
+for (let i = 0; i < attack.length; i++) {
+  let card = document.createElement('div')
+  card.className = 'attackCard'
+  let num_poke = document.createElement('h4')
+  num_poke.textContent = 'N° ' + attack[i].num
+  let img_attack = document.createElement('img')
+  img_attack.src = attack[i].img
+
+  let litleAttack = document.createElement('h2')
   litleAttack.textContent = attack[i].name
-   let num_attack= document.createElement("h3")
-  num_attack.textContent="Base Ataque : " + attack[i].stats['base-attack'] 
-  
+  let num_attack = document.createElement('h3')
+  num_attack.textContent = 'Base Ataque : ' + attack[i].stats['base-attack']
+
   card.appendChild(num_poke)
   card.appendChild(img_attack)
   card.appendChild(litleAttack)
   card.appendChild(num_attack)
 
   viewAttack.appendChild(card)
-  
-  console.log(litleAttack)
-  console.log(img_attack)
+
 }
-
 //(captura el valor que se ingresa al input)
-document.querySelector("#searchName").addEventListener("keyup", (event) => {
+document.querySelector('#searchName').addEventListener('keyup', (event) => {
 
-  const listaFiltrada = pokemonData.filter(poke => poke.name.startsWith(event.target.value))
-  let list2 = document.getElementById("list_pokemon")
+  const listFilter = pokemonData.filter(poke => poke.name.startsWith(event.target.value))
+  let list2 = document.getElementById('list_pokemon')
   list2.innerHTML = ''
 
-  console.log(listaFiltrada)
-
-  for (let i = 0; i < listaFiltrada.length; i++) {
-    let list = document.getElementById("list_pokemon")
-    let list_name = document.createElement("li")
-    let info = document.createElement("div")
-    let name_poke = document.createElement("h2")
-    name_poke.textContent = listaFiltrada[i].name
-    let num_poke = document.createElement("h3")
-    num_poke.textContent = "N° " + listaFiltrada[i].num
+  if (event.target.value.length === 0 && event.key === 'Backspace') return
+  for (let i = 0; i < listFilter.length; i++) {
+    let list = document.getElementById('list_pokemon')
+    let list_name = document.createElement('li')
+    let info = document.createElement('div')
+    let name_poke = document.createElement('h2')
+    name_poke.textContent = listFilter[i].name
+    let num_poke = document.createElement('h3')
+    num_poke.textContent = 'N° ' + listFilter[i].num
     // list_name.addEventListener('click', letterPokemon)
-    let img_poke = document.createElement("img")
-    img_poke.src = listaFiltrada[i].img
+    let img_poke = document.createElement('img')
+    img_poke.src = listFilter[i].img
     info.appendChild(name_poke)
     info.appendChild(num_poke)
     list_name.appendChild(info)
     list_name.appendChild(img_poke)
     list.appendChild(list_name)
-   
   }
 }
 )
